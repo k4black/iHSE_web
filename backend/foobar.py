362444392344b7c2b3ea0cacdc50fda9b2e7017b
Ubuntu <ubuntu@ip-172-31-36-110.us-east-2.compute.internal>
2019-04-08 06:29:39 +0000

    50000th port version

--- backend/foobar.py
+++ backend/foobar.py
@@ -7,7 +7,8 @@ def application(env, start_response):
     start_response('200 OK',
                    [('Access-Control-Allow-Origin', '*'),
                     ('Content-type', 'text/html'),
-                    ('Content-Length', str(len(message_return)))])
+                    # ('Content-Length', str(len(message_return)))
+                    ])
     # open("/tmp/ot.txt", "w").write(message_return)
     # ret = open("/tmp/ot.txt").read()
     return [message_return]
