0bf03a4e3b50f7967506dbde9593d984a581b1c2
k4black <k4black@ya.ru>
2019-04-08 09:13:48 +0300

    Refactoring

--- scripts/uWSGI_test.js
+++ scripts/uWSGI_test.js
@@ -1,14 +1,22 @@
 function loadDoc() {
+    console.log("Change content");
+
     var xhttp = new XMLHttpRequest();
+
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
             document.getElementById("demo").innerHTML = this.responseText;
+
         } else if (this.readyState === 4) {
             document.getElementById("demo").innerHTML = "status: " + this.status.toString();
+
         } else if (this.status === 200) {
             document.getElementById("demo").innerHTML = "readyState: " + this.readyState.toString();
+        } else {
+            console.log("Error!");
         }
     };
-    xhttp.open("GET", "http://ihse.tk:3030", true);
+
+    xhttp.open("GET", "http://ihse.tk:5000", true);
     xhttp.send();
 }
