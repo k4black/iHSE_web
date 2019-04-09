497cec04841787a00063e864fccbf218a2113c08
k4black <k4black@ya.ru>
2019-04-10 00:24:46 +0300

    Login form

---
+++ scripts/login.js
@@ -0,0 +1,82 @@
+var mobileNav = document.querySelector('.arrow__button');
+mobileNav.addEventListener('click', back);
+
+function back() {
+    //alert(document.referrer);
+    window.location.href = document.referrer;
+}
+
+
+
+
+
+
+console.log("!!!");
+
+var button = document.querySelector('#btn');
+
+button.addEventListener('click', btn);
+
+function btn() {
+    console.log("result_form");
+    sendAjaxForm('result_form', 'ajax_form', 'action_ajax_form.php');
+    return false;
+}
+
+
+function sendAjaxForm(result_form, ajax_form, url) {
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.status === 200) {
+            console.log("200 ok!");
+        } else {
+            console.log("Smth else!");
+        }
+    };
+
+    xhttp.timeout = 30000;
+    xhttp.ontimeout = function() {
+        alert( 'Too loooooong' );
+    };
+
+    xhttp.open("POST", "http://ihse.tk:50000/path/resource?param1=value1&param2=value2", true, "user", "pass");
+    xhttp.send();
+}
+
+
+
+
+
+
+console.log('Login form');
+
+var name = document.querySelector('#name');
+var pass = document.querySelector('#pass');
+
+
+pass.addEventListener('focus', function () {
+    pass.closest('div').querySelector("label").classList.add('active');
+    console.log('Pass active');
+});
+
+pass.addEventListener('blur', function () {
+    if (pass.value != "")
+        return;
+
+    pass.closest('div').querySelector("label").classList.remove('active');
+    console.log('Pass inactive');
+});
+
+
+name.addEventListener('focus', function () {
+    name.closest('div').querySelector("label").classList.add('active');
+    console.log('Name active');
+});
+
+name.addEventListener('blur', function () {
+    name.closest('div').querySelector("label").classList.remove('active');
+    console.log('Name inactive');
+});
+
