c8b478fca8610e7787f3d021ec0424afc1f370f5
Maksim Makridin <maginot.fireline@gmail.com>
2019-04-17 12:03:38 +0300

    GSheetsAPI test

--- backend/foobar.py
+++ backend/foobar.py
@@ -133,8 +133,8 @@ def gsheets_get_day() -> list:
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists('/home/ubuntu/iHSE_web/backend/token.pickle'):
+        with open('/home/ubuntu/iHSE_web/backend/token.pickle', 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -145,7 +145,7 @@ def gsheets_get_day() -> list:
             flow = InstalledAppFlow.from_client_secrets_file('/home/ubuntu/iHSE_web/backend/credentials.json', SCOPES)
             creds = flow.run_local_server()
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open('/home/ubuntu/iHSE_web/backend/token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('sheets', 'v4', credentials=creds)
