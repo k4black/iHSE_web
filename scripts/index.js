15d71a5dbaca6f9e9bcc5994e054b87c49506944
Konstantin Chernyshev <k4black@ya.ru>
2019-05-27 22:33:44 +0300

    Today for python backend , index and calendar

--- scripts/index.js
+++ scripts/index.js
@@ -91,7 +91,7 @@ var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 
-if (today.getDate() < 5 && today.getMonth() + 1 < 6)
+if (today.getDate() < 5 || today.getMonth() + 1 < 6)
     today = '05.06';
 else
     today = dd + '.' + mm;
