64021083d7bfba7f57304841a43703bcdf14dd45
Konstantin Chernyshev <k4black@ya.ru>
2019-12-06 19:16:08 +0300

    Added some UI for admin panel

--- scripts/admin.js
+++ scripts/admin.js
@@ -10,47 +10,44 @@
 
 Vue.use(Vuetable);
 new Vue({
-  el: '#app',
-  components:{
-   'vuetable-pagination': Vuetable.VuetablePagination
-  },
-  data: {
-    fields: ['name', 'email','birthdate','nickname','gender','__slot:actions'],
-    filterText: ''
-  },
-  computed:{
-  /*httpOptions(){
-    return {headers: {'Authorization': "my-token"}} //table props -> :http-options="httpOptions"
-  },*/
- },
- methods: {
-    onPaginationData (paginationData) {
-    this.$refs.paginationTop.setPaginationData(paginationData);      // <----
-    this.$refs.paginationInfoTop.setPaginationData(paginationData);  // <----
+    el: '#app',
+    components:{
+        'vuetable-pagination': Vuetable.VuetablePagination
+    },
+    data: {
+        fields: ['name', 'email','birthdate','nickname','gender','__slot:actions'],
+        filterText: ''
+    },
+    computed:{
+        /*httpOptions(){
+        return {headers: {'Authorization': "my-token"}} //table props -> :http-options="httpOptions"
+        },*/
+    },
+    methods: {
+        onPaginationData (paginationData) {
+            this.$refs.paginationTop.setPaginationData(paginationData);
+            this.$refs.paginationInfoTop.setPaginationData(paginationData);
+        },
+        onChangePage (page) {
+            this.$refs.vuetable.changePage(page)
+        },
+        editRow(rowData){
+            alert("You clicked edit on"+ JSON.stringify(rowData))
+        },
+        deleteRow(rowData){
+            alert("You clicked delete on"+ JSON.stringify(rowData))
+        },
 
-    this.$refs.pagination.setPaginationData(paginationData);
-    this.$refs.paginationInfo.setPaginationData(paginationData);
-  },
-    onChangePage (page) {
-      this.$refs.vuetable.changePage(page)
-    },
-    editRow(rowData){
-      alert("You clicked edit on"+ JSON.stringify(rowData))
-    },
-    deleteRow(rowData){
-      alert("You clicked delete on"+ JSON.stringify(rowData))
-    },
-
-    doFilter () {
-        console.log('doFilter:', this.filterText);
-        alert("You doFilter"+ JSON.stringify(this.filterText))
-      },
-     resetFilter () {
-        this.filterText = '';
-        console.log('resetFilter');
-        alert("resetFilter")
-      }
-  }
+        doFilter () {
+            console.log('doFilter:', this.filterText);
+            alert("You doFilter"+ JSON.stringify(this.filterText))
+        },
+        resetFilter () {
+            this.filterText = '';
+            console.log('resetFilter');
+            alert("resetFilter")
+        }
+    }
 });
 
   //
