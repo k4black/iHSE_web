ca5192752b9ac7bf5caf41cfa0be48d6d9d309ff
Konstantin Chernyshev <k4black@ya.ru>
2019-12-09 16:06:41 +0300

    Refactoring of admin And more things on it

--- scripts/admin.js
+++ scripts/admin.js
@@ -4,21 +4,18 @@
  * Including animations and http requests if that are sending by NOT nav elements
  */
 
-$('.openmodal').click(function (e) {
-  e.preventDefault();
-  $('.kadobagud').addClass('midsalod');
-  });
-$('.closemodal').click(function (e) {
-  e.preventDefault();
-  $('.kadobagud').removeClass('midsalod');
-  });
+
+
+var $table = $('#table');
+var $popup = $('#popup');
+var $popup_inputs = $('#popup .fields');
+console.log($popup_inputs);
+var $button = document.getElementsByName("refresh");
 
 
 
 
- var $table = $('#table');
-
-    var data = [
+var data = [
     {
         "id": 0,
         "name": "test0",
@@ -125,82 +122,171 @@ $('.closemodal').click(function (e) {
         "price": "$20"
     }
 ];
+var fields = ['id', 'name', 'price'];
 
-$(function () {
-    $table.bootstrapTable({
-        data: data,
-        columns: [{
-        title: 'ID',
-        field: 'id',
+var columns = [];  // TODO: columns name map
+for (let i = 0; i < fields.length; ++i) {
+    columns.push({
+        title: fields[i],
+        field: fields[i],
         sortable: 'true'
-      }, {
-        title: 'Item Name',
-        field: 'name',
-        sortable: 'true'
-      }, {
-        title: 'Item Price',
-        field: 'price'
-      }, {
-        title: 'Actions',
-        field: 'operate',
-        formatter: 'operateFormatter',
-        events: 'operateEvents',
-        width: '200'
-      }]
     });
+}
+
+columns.push({
+    title: 'Actions',
+    field: 'operate',
+    formatter: 'operateFormatter',
+    events: 'operateEvents',
+    width: '200'
 });
 
-  function operateFormatter(value, row, index) {
+// var columns = [{
+//         title: 'ID',
+//         field: 'id',
+//         sortable: 'true'
+//         }, {
+//         title: 'Item Name',
+//         field: 'name',
+//         sortable: 'true'
+//       }, {
+//         title: 'Item Price',
+//         field: 'price'
+//       }, {
+//         title: 'Actions',
+//         field: 'operate',
+//         formatter: 'operateFormatter',
+//         events: 'operateEvents',
+//         width: '200'
+//       }
+// ];
+
+
+
+
+function operateFormatter(value, row, index) {
     return [
-      '<button class="pencil" href="javascript:void(0)" title="Edit">',
-      '<i class="fa fa-wrench"></i> ',
+        '<button class="edit" href="javascript:void(0)" title="Edit">',
+        '<i class="fa fa-wrench"></i> ',
         'Edit',
-      '</button>',
-      '<button class="remove danger_button" href="javascript:void(0)" title="Remove">',
-      '<i class="fa fa-trash"></i> ',
+        '</button>',
+        '<button class="remove danger_button" href="javascript:void(0)" title="Remove">',
+        '<i class="fa fa-trash"></i> ',
         'Remove',
-      '</button>'
+        '</button>'
     ].join('')
-  }
+}
 
-  window.operateEvents = {
-    'click .pencil': function (e, value, row, index) {
-        // $table.SetEditable({ $addButton: $('#addNewRow')});
-
-      alert('You click like action, row: ' + JSON.stringify(row));
-      tmp_row = {
-        "id": 123123,
+function update_row(index, id) {
+    tmp_row = {
+        "id": id * id,
         "name": "test1000",
         "price": "111$0"
-        };
+    };
 
-      $table.bootstrapTable('updateRow', {index: index, row: tmp_row});
-      $('#popup').fadeIn(350);
-      $('#popup').fadeOut(350);
+    console.log($popup_inputs);
+
+    for (let i = 0; i < fields.length; ++i) {
+        console.log(fields[i], $popup_inputs.querySelector('input[name=' + fields[i] + ']'));
+        tmp_row[fields[i]] = $popup_inputs.querySelector('input[name=' + fields[i] + ']').value;
+    }
+
+    $table.bootstrapTable('updateRow', {index: index, row: tmp_row});
+}
+
+function create_row() {
+    tmp_row = {
+        "id": id * id,
+        "name": "test1000",
+        "price": "111$0"
+    };
+
+    console.log($popup_inputs);
+
+    for (let i = 0; i < fields.length; ++i) {
+        console.log(fields[i], $popup_inputs.querySelector('input[name=' + fields[i] + ']'));
+        tmp_row[fields[i]] = $popup_inputs.querySelector('input[name=' + fields[i] + ']').value;
+    }
+
+    $table.bootstrapTable('append', tmp_row);
+}
+
+function edit_row(row) {
+    console.log(row);
+
+    $popup_inputs.innerHTML = '';
+    let inputs_html = '';
+    for (let i = 0; i < columns.length; ++i) {
+        if (columns[i]['field'] === 'operate') {
+            break;
+        }
+
+        console.log(columns[i]['title'], columns[i]['field'], row[columns[i]['field']]);
+
+        let current_inputs_html = '<label for=' + columns[i]['title'] + '>' + columns[i]['title'] + '</label>';
+        if (Object.keys(row).length === 0) {
+            current_inputs_html += '<input name=' + columns[i]['title'] + ' value="" type="text">';
+        } else {
+            current_inputs_html += '<input name=' + columns[i]['title'] + ' value="' + row[columns[i]['field']] + '" type="text">';
+        }
+        inputs_html += '<div style="display: block">' + current_inputs_html + '</div>';
+    }
+
+    $popup_inputs.innerHTML = inputs_html;
+}
+
+
+
+// Add buttons Edin and Remove
+window.operateEvents = {
+    'click .edit': function (e, value, row, index) {
+        // alert('You click like action, row: ' + JSON.stringify(row));
+
+        $popup.attr('row_index', index);
+        $popup.attr('row_id', row["id"]);
+        $popup.attr('row_edit', true);
+
+        edit_row(row);
+
+
+
+        console.log($popup.attr('row_index'));
+        console.log($popup.attr('row_id'));
+
+        $popup.fadeIn(350);
     },
     'click .remove': function (e, value, row, index) {
-      alert('You click like action, row: ' + JSON.stringify(row));
-      $table.bootstrapTable('remove', {
-        field: 'id',
-        values: [row.id]
-      })
+        // alert('You click like action, row: ' + JSON.stringify(row));
+
+        if (confirm("Are you sure? You wand to remove: \n" + JSON.stringify(row))) {
+            $table.bootstrapTable('remove', {
+                field: 'id',
+                values: [row.id]
+                })
+        } else {
+            // pass
+        }
     }
-  };
+};
 
+$(function () {
 
-    var $button = document.getElementsByName("refresh");
-    // $button = $button.item(0);
-        console.log($button);
+    // Creating table  - adding data and columns
+    $table.bootstrapTable({
+        data: data,
+        columns: columns
+    });
 
-  $(document).ready(function() {
-        console.log('Ready');
-        $button = $button.item(0);
-        console.log($button);
+    $popup_inputs = $popup_inputs[0];
+    console.log($popup_inputs);
+
+    $button = $button.item(0);
 
     $button.onclick = (function () {
         console.log('refresh');
 
-        data = [    {
+        data = [
+        {
             "id": 19,
             "name": "test19",
             "price": "$19"
@@ -214,10 +300,18 @@ $(function () {
         $table.bootstrapTable('load',  data);
         // $table.bootstrapTable('refresh')
 
-    })
+    });
+
+    $('#addNewRow')[0].onclick = (function () {
+        console.log('Adding new row');
+
+        $popup.attr('row_edit', false);
+        edit_row({});
+        $popup.fadeIn(350);
+    });
+
   });
 
-$table.SetEditable({ $addButton: $('#addNewRow')});
 
 /** ===============  LOGIC and REQUESTS  =============== */
 
