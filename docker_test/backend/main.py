7b426ad6037302735f8d4e0ce16a5258f757e76f
Konstantin Chernyshev <k4black@ya.ru>
2020-02-08 20:57:29 +0300

    [WIP] Minor improves

--- docker_test/backend/main.py
+++ docker_test/backend/main.py
@@ -3,4 +3,4 @@
 
 def application(environ, start_response):
     start_response('200 damn', [('Content-Type', 'text/html')])
-    return ["Ololo!".encode("utf-8")]
+    return [(f"Ololo! YOu want to {environ['PATH_INFO']}").encode("utf-8")]
