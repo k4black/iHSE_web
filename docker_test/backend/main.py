fd3dfc9ddf40fdfac1346ee849145920721c8005
Konstantin Chernyshev <k4black@ya.ru>
2020-02-09 15:51:24 +0300

    [WIP] Added database

--- docker_test/backend/main.py
+++ docker_test/backend/main.py
@@ -1,4 +1,10 @@
 
+import psycopg2
+
+# conn = psycopg2.connect('dbname=root user=root password=root',port=5432)
+conn = psycopg2.connect(database="root", user="root", password="root", host="database_docker", port="5432")
+
+cursor = conn.cursor()
 
 
 def application(environ, start_response):
--- docker_test/uwsgi.dockerfile
+++ docker_test/uwsgi.dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.7
-RUN pip install uwsgi
+RUN pip install uwsgi && pip install psycopg2-binary
 
 EXPOSE 8001
 
