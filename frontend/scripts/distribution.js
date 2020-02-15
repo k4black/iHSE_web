53610422b447bbc269b8f17c89ddde3e3dc43f0b
Konstantin Chernyshev <k4black@ya.ru>
2020-02-15 10:34:18 +0300

    [WIP] Distribution page pie-s

---
+++ frontend/scripts/distribution.js
@@ -0,0 +1,115 @@
+
+
+
+$(function() {
+    current_timeline = 'vacations';
+    // loadAndCreateTable(current_table);
+
+    // setupToolbar();
+    // setupTabs();
+    loadDays(function () {checkLoading(setDistribution, ['vacations', 'users', 'days'])});
+    loadUsers(function () {checkLoading(setDistribution, ['vacations', 'users', 'days'])});
+    loadVacations(function () {checkLoading(setDistribution, ['vacations', 'users', 'days'])});
+
+    // buildTestTimeline();
+});
+
+
+
+
+function setDistribution() {
+
+    let users_by_id = cache['users'];
+
+    let users = groupBy(Object.values(users_by_id), 'team');
+
+
+    console.log('teams dist', users);
+
+
+    let teams_html = '';
+    for (let team in users) {
+        teams_html += '<div id="team' + team + '"></div>'
+    }
+
+    document.getElementById('teams').innerHTML = teams_html;
+
+
+    var ctx = document.getElementById('team0').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45]
+            }]
+        },
+
+        // Configuration options go here
+        options: {}
+    });
+}
+
+
+
+
+/**
+ * Get user information from server
+ * Save list to global 'cache['users']'
+ *
+ * Run func on OK status
+ */
+function loadUsers(func) {
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if (this.status === 200) { // If ok set up fields
+                let users = JSON.parse(this.responseText);
+                let objs = groupByUnique(users, 'code');
+
+                cache['users'] = objs;
+
+                func();
+            }
+        }
+    };
+
+    xhttp.open("GET", "//ihse.tk/admin_get_table?" + "table=" + 'users', true);
+    xhttp.withCredentials = true; // To send Cookie;
+    xhttp.send();
+}
+
+
+/**
+ * Get vacatios information from server
+ * Save list to global 'cache['vacations']'
+ *
+ * Run func on OK status
+ */
+function loadVacations(func) {
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if (this.status === 200) { // If ok set up fields
+                let vacations = JSON.parse(this.responseText);
+                let objs = groupByUnique(vacations, 'code');
+
+                cache['vacations'] = objs;
+
+                func();
+            }
+        }
+    };
+
+    xhttp.open("GET", "//ihse.tk/admin_get_table?" + "table=" + 'vacations', true);
+    xhttp.withCredentials = true; // To send Cookie;
+    xhttp.send();
+}
