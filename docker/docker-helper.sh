5bf216330fd2cdb9da6d3019c0755bb2a89bdae0
Konstantin Chernyshev <k4black@ya.ru>
2020-06-01 19:36:42 +0300

    Added command line parsing for docker-helper.sh

--- docker/docker-helper.sh
+++ docker/docker-helper.sh
@@ -22,12 +22,12 @@
 
 
 function help {
-    echo "usage: docker-helper.sh start/stop [-l local] [-d demonized]"
+    echo "usage: docker-helper.sh start/stop [-l local] [-d demonized] [-f file] [-h help]"
 }
 
 
 
-# Check command  
+# Check command
 command=$1;  shift ;
 if [ ! "$command" = "start" ] &&  [ ! "$command" = "stop" ] ; then
     echo "Unknown command $command."
@@ -41,6 +41,7 @@ while [[ "$#" -gt 0 ]]; do
     case $1 in
         -l|--local) local=true ;;
         -d|--demonized) demonized=true ;;
+        -f|--file) file="$2"; shift ;;
         -h|--help) help; exit 0 ;;
         *) echo "Unknown argument $1."; help; exit 3 ;;
     esac
@@ -49,47 +50,51 @@ done
 
 
 
-
+# check permissions
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root/sudo"
   exit 0
 fi
 
+
+# Choose compose
 conf_file="./docker-compose.yml"
 if [ "$local" = true ] ; then
     conf_file="./docker-compose-local.yml"
 fi
+if [ ! -z $file ] ; then
+    conf_file="$file"
+fi
 
+
+# Run docker-compose 
 if [ "$command" = "start" ]
     then echo "starting..."
-    if [ -d "../local_run" ]
-        then echo "local_run exists, setting permissions..."
-        chown -R 1000:0 ../local_run/logstash
-        chown -R 1000:0 ../local_run/elasticsearch
-        chown -R 999:999 ../local_run/postgres
-        chown -R 0:0 ../local_run/nginx
-        chown -R 0:0 ../local_run/uwsgi
-        echo "permission set"
-    fi
-    if [ ! -d "../local_run" ]
-        then echo "local_run doesn't exist, creating..."
-        mkdir -p ../local_run/logstash/data
-        mkdir -p ../local_run/elasticsearch/data
-        mkdir -p ../local_run/elasticsearch/logs
-        mkdir -p ../local_run/postgres/pgdata
-        mkdir -p ../local_run/postgres/exports
-        mkdir -p ../local_run/nginx/logs
-        mkdir -p ../local_run/uwsgi/logs
-        echo "local_run created, setting permissions..."
-        chown -R 1000:0 ../local_run/logstash
-        chown -R 1000:0 ../local_run/elasticsearch
-        chown -R 999:999 ../local_run/postgres
-        chown -R 0:0 ../local_run/nginx
-        chown -R 0:0 ../local_run/uwsgi
-        echo "permission set"
-    fi
-    echo "starting Compose..."
 
+    if [ "$local" = true ] ; then
+        if [ ! -d "../local_run" ] ; then
+            echo "local_run doesn't exist, creating..."
+            mkdir -p ../local_run/logstash/data
+            mkdir -p ../local_run/elasticsearch/data
+            mkdir -p ../local_run/elasticsearch/logs
+            mkdir -p ../local_run/postgres/pgdata
+            mkdir -p ../local_run/postgres/exports
+            mkdir -p ../local_run/nginx/logs
+            mkdir -p ../local_run/uwsgi/logs
+            echo "local_run created, setting permissions..."
+        else
+            echo "local_run exists, setting permissions..."
+        else
+
+        chown -R 1000:0 ../local_run/logstash
+        chown -R 1000:0 ../local_run/elasticsearch
+        chown -R 999:999 ../local_run/postgres
+        chown -R 0:0 ../local_run/nginx
+        chown -R 0:0 ../local_run/uwsgi
+        echo "permission set"
+    fi
+
+    echo "starting Compose..."
     if [ "$demonized" = true ] ; then
         docker-compose -f "$conf_file" up -d
         echo "demonized."
